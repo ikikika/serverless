59a8cc27478eeda785fdd4d061c9610a2f1dd118
ikikika <iki.jito@gmail.com>
2018-11-05 16:43:09 +0800

    highlight link when clicked, no page refresh

--- notes-app-client/src/App.js
+++ notes-app-client/src/App.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import "./App.css";
 import Routes from "./Routes";
+import { LinkContainer } from "react-router-bootstrap";
 
 class App extends Component {
   render() {
@@ -17,8 +18,12 @@ class App extends Component {
           </Navbar.Header>
           <Navbar.Collapse>
             <Nav pullRight>
-              <NavItem href="/signup">Signup</NavItem>
-              <NavItem href="/login">Login</NavItem>
+              <LinkContainer to="/signup">
+                <NavItem>Signup</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <NavItem>Login</NavItem>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Navbar>

dd800998bc3c2ad465e2835db654ef9e709546f3
ikikika <iki.jito@gmail.com>
2018-12-01 18:05:59 +0800

    set up individual notes page

--- notes-app-client/src/Routes.js
+++ notes-app-client/src/Routes.js
@@ -6,12 +6,20 @@ import Login from "./containers/Login";
 import AppliedRoute from "./components/AppliedRoute";
 import Signup from "./containers/Signup";
 import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
 
 export default ({ childProps }) => (
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
     <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
+    <AppliedRoute
+      path="/notes/:id"
+      exact
+      component={Notes}
+      props={childProps}
+    />
+
     <AppliedRoute
       path="/notes/new"
       exact

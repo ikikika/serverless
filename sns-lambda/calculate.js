ec63eec20a076dd9ad02bc0ab264c299ced015ae
ikikika <iki.jito@gmail.com>
2019-02-26 18:51:23 +0800

    codes for testing sns and lambda

---
+++ sns-lambda/calculate.js
@@ -0,0 +1,9 @@
+// calculate.js
+module.exports.handler = async event => {
+  const { number } = JSON.parse(event.Records[0].Sns.Message);
+  const factorial = x => (x === 0 ? 1 : x * factorial(x - 1));
+  const result = factorial(number);
+
+  console.log(`The factorial of ${number} is ${result}.`);
+  return result;
+};

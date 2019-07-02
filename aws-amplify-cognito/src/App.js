1cff0e78e4106feb8ee6cf86cb3f55a6ec3090e6
ikikika <iki.jito@gmail.com>
2019-07-03 01:19:09 +0800

    session persistence

--- aws-amplify-cognito/src/App.js
+++ aws-amplify-cognito/src/App.js
@@ -15,12 +15,14 @@ import Welcome from "./components/auth/Welcome";
 import Footer from "./components/Footer";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import Auth from "@aws-amplify/auth";
 import Analytics from "@aws-amplify/analytics";
 library.add(faEdit);
 
 class App extends Component {
   state = {
     isAuthenticated: false,
+    isAuthenticating: true,
     user: null
   };
 
@@ -32,6 +34,22 @@ class App extends Component {
     this.setState({ user: user });
   };
 
+  async componentDidMount() {
+    try {
+      const session = await Auth.currentSession();
+      this.setAuthStatus(true);
+      console.log(session);
+      const user = await Auth.currentAuthenticatedUser();
+      this.setUser(user);
+    } catch (error) {
+      if (error !== "No current user") {
+        console.log(error);
+      }
+    }
+
+    this.setState({ isAuthenticating: false });
+  }
+
   render() {
     Analytics.disable();
     const authProps = {
@@ -41,70 +59,76 @@ class App extends Component {
       setUser: this.setUser
     };
     return (
-      <div className="App">
-        <Router>
-          <div>
-            <Navbar auth={authProps} />
-            <Switch>
-              <Route
-                exact
-                path="/"
-                render={props => <Home {...props} auth={authProps} />}
-              />
-              <Route
-                exact
-                path="/products"
-                render={props => <Products {...props} auth={authProps} />}
-              />
-              <Route
-                exact
-                path="/admin"
-                render={props => <ProductAdmin {...props} auth={authProps} />}
-              />
-              <Route
-                exact
-                path="/login"
-                render={props => <LogIn {...props} auth={authProps} />}
-              />
-              <Route
-                exact
-                path="/register"
-                render={props => <Register {...props} auth={authProps} />}
-              />
-              <Route
-                exact
-                path="/forgotpassword"
-                render={props => <ForgotPassword {...props} auth={authProps} />}
-              />
-              <Route
-                exact
-                path="/forgotpasswordverification"
-                render={props => (
-                  <ForgotPasswordVerification {...props} auth={authProps} />
-                )}
-              />
-              <Route
-                exact
-                path="/changepassword"
-                render={props => <ChangePassword {...props} auth={authProps} />}
-              />
-              <Route
-                exact
-                path="/changepasswordconfirmation"
-                render={props => (
-                  <ChangePasswordConfirm {...props} auth={authProps} />
-                )}
-              />
-              <Route
-                exact
-                path="/welcome"
-                render={props => <Welcome {...props} auth={authProps} />}
-              />
-            </Switch>
-            <Footer />
-          </div>
-        </Router>
-      </div>
+      !this.state.isAuthenticating && (
+        <div className="App">
+          <Router>
+            <div>
+              <Navbar auth={authProps} />
+              <Switch>
+                <Route
+                  exact
+                  path="/"
+                  render={props => <Home {...props} auth={authProps} />}
+                />
+                <Route
+                  exact
+                  path="/products"
+                  render={props => <Products {...props} auth={authProps} />}
+                />
+                <Route
+                  exact
+                  path="/admin"
+                  render={props => <ProductAdmin {...props} auth={authProps} />}
+                />
+                <Route
+                  exact
+                  path="/login"
+                  render={props => <LogIn {...props} auth={authProps} />}
+                />
+                <Route
+                  exact
+                  path="/register"
+                  render={props => <Register {...props} auth={authProps} />}
+                />
+                <Route
+                  exact
+                  path="/forgotpassword"
+                  render={props => (
+                    <ForgotPassword {...props} auth={authProps} />
+                  )}
+                />
+                <Route
+                  exact
+                  path="/forgotpasswordverification"
+                  render={props => (
+                    <ForgotPasswordVerification {...props} auth={authProps} />
+                  )}
+                />
+                <Route
+                  exact
+                  path="/changepassword"
+                  render={props => (
+                    <ChangePassword {...props} auth={authProps} />
+                  )}
+                />
+                <Route
+                  exact
+                  path="/changepasswordconfirmation"
+                  render={props => (
+                    <ChangePasswordConfirm {...props} auth={authProps} />
+                  )}
+                />
+                <Route
+                  exact
+                  path="/welcome"
+                  render={props => <Welcome {...props} auth={authProps} />}
+                />
+              </Switch>
+              <Footer />
+            </div>
+          </Router>
+        </div>
+      )
     );
   }
 }
